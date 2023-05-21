7675a254429bcc2eb8aa8f0b1894d86a873889ec
AsrarOthman <asrarothman@gmail.com>
2023-05-21 10:54:18 +0800

    checklist

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const CheckListApp = () => {
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState("");
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (text.trim() !== "") {
+      const newItem = {
+        id: new Date().getTime(),
+        text: text.trim(),
+        checked: false,
+      };
+
+      setItems([...items, newItem]);
+      setText("");
+    }
+  };
+
+  const handleToggleCheck = (itemId) => {
+    setItems((prevItems) =>
+      prevItems.map((item) => {
+        if (item.id === itemId) {
+          return { ...item, checked: !item.checked };
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleDeleteItem = (itemId) => {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Asrar Othman love Ameela</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+      }}
+    >
+      <h1 style={{ fontFamily: "cursive", marginTop: 30 }}>Ameela Niqabisku</h1>
+      <h1 style={{ fontFamily: "Arial", marginTop: 30 }}>Check List App</h1>
 
-export default App;
+      <div style={{ marginTop: 20 }}>
+        <input
+          type="text"
+          value={text}
+          onChange={handleTextChange}
+          placeholder="Enter an item"
+        />
+        <button onClick={handleAddItem}>Add</button>
+      </div>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.checked}
+              onChange={() => handleToggleCheck(item.id)}
+            />
+            <span
+              style={item.checked ? { textDecoration: "line-through" } : {}}
+            >
+              {item.text}
+            </span>
+            <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CheckListApp;
